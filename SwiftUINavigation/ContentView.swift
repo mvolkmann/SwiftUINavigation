6c20622da2386a93d958332d5ebe60328118067e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-23 08:09:03 -0500

    initial version

--- SwiftUINavigation/ContentView.swift
+++ SwiftUINavigation/ContentView.swift
@@ -1,16 +1,53 @@
-//
-//  ContentView.swift
-//  SwiftUINavigation
-//
-//  Created by R. Mark Volkmann on 10/23/21.
-//
-
 import SwiftUI
+
+struct Page1: View {
+    var body: some View {
+        VStack {
+            Text("This is page one.")
+                .navigationTitle("Page 1")
+                .navigationBarTitleDisplayMode(.inline) // .large leaves too much space above
+            Spacer()
+            HStack {
+                NavigationLink(destination: Page2()) {
+                    Text("Page 2")
+                }
+                NavigationLink(destination: Page3()) {
+                    Text("Page 3")
+                }
+            }
+        }
+    }
+}
+
+struct Page2: View {
+    var body: some View {
+        Text("This is page two.").navigationTitle("Page 2")
+    }
+}
+
+struct Page3: View {
+    var body: some View {
+        VStack {
+            Text("This is page three.").navigationTitle("Page 3")
+            Spacer()
+            NavigationLink(destination: Page4()) {
+                Text("Page 4")
+            }
+        }
+    }
+}
+
+struct Page4: View {
+    var body: some View {
+        Text("This is page four.").navigationTitle("Page 4")
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Page1()
+        }
     }
 }
 
